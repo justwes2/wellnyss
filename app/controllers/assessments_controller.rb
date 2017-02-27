6e3d12d105034e1ff71651faf1979687a970d696
Wes <wes.coffay@gmail.com>
2017-02-27 10:32:13 -0500

    issues with method controllers

--- app/controllers/assessments_controller.rb
+++ app/controllers/assessments_controller.rb
@@ -1,2 +1,25 @@
 class AssessmentsController < ApplicationController
+
+  def index
+    @assessments = Assessment.find(params[:user_id])
+  end
+
+  def show
+    @assessment = Assessment.find(params[:id])
+  end
+
+  def new
+    @assessment = Assessment.new
+  end
+
+  def create
+    @assessment= Assessment.new
+    @assessment.save
+    redirect_to user_assessments
+  end
+
+  private
+  def assessment_params
+    params.require(:assessment).permit(:score)
+  end
 end
