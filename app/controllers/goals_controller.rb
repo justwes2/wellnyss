f86453bf0d439286dda4ca7677805aa55ec59bca
Wes <wes.coffay@gmail.com>
2017-02-27 16:09:45 -0500

    CRUD up for all three data tables

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -28,7 +28,7 @@ class GoalsController < ApplicationController
   def update
     @goal = Goal.find(params[:id])
     @goal.update(goal_params)
-    redirect_to @goal
+    redirect_to user_goals_url(current_user)
   end
 
   def destroy
